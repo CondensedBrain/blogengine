ba4a83c37c5a96805f5291e58350c9130fa0980c
Michael Robinson <condensedbrain@gmail.com>
2019-02-24 16:25:05 -0500

    Add post saving logic.

---
+++ draft-post.php
@@ -0,0 +1,1 @@
+TODO: Create new file with variables set to default thing and the current unix time in the post time.
--- render.php
+++ render.php
@@ -1,12 +1,22 @@
 <?php
-
+//Settings
 $site_name = "Condensed Brain";
+date_default_timezone_set('America/New_York');
+
 
 
 foreach (glob("posts/*.php") as $filename)
 {
+ob_start();
 require $filename;
 
 require "template.php";
+  $content = ob_get_contents();
+  // Worry about permalink/filename/directories later
+  // Will be like /post-slug/index.html
+ file_put_contents( "./output/" . $filename . "-rendered.html", $content);
+
+
+  ob_end_clean();
 }
 ?>
